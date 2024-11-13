4ab0ace405a65d9f94bceece2e70643cbedf1580
Sizwe <Sizwe@example.com>
2024-11-13 12:16:51 +0200

    feat(17-custom-actions): add logic to create PR

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,15 +1,20 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
 const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(dirName);
-async function run() {
-    const baseBranch = core.getInput('base-branch');
-    const targetBranch = core.getInput('target-branch');
-    const ghToken = core.getInput('gh-token');
-    const workingDir = core.getInput('working-directory');
-    const debug = core.getBooleanInput('debug');
 
+async function run() {
+    const baseBranch = core.getInput('base-branch', { required: true });
+    const targetBranch = core.getInput('target-branch', { required: true });
+    const ghToken = core.getInput('gh-token', { required: true });
+    const workingDir = core.getInput('working-directory', { required: true });
+    const debug = core.getBooleanInput('debug');
+  
+    const commonExecOpts = {
+      cwd: workingDir,
+    }
     core.setSecret(ghToken);
 
     if (!validateBranchName({ branchName: baseBranch })) {
@@ -32,17 +37,49 @@ async function run() {
     core.info('[js-dependency-update] : working directory is ${workingDir');
 
     await exec.exec('npm update', [], {
-      cwd: workingDir   
+      ...commonExecOpts   
     });
 
-    const gitStatus = await exec.getExecOutput('git status -s package*json', [], {
-      cwd: workingDir
+    const gitStatus = await exec.getExecOutput('git status -s package*json', [], 
+      {
+      ...commonExecOpts
     });
 
     if (gitStatus.stdout.length > 0) {
-      core.info('[js-dependency-update] : There are updates available!')
+      core.info('[js-dependency-update] : There are updates available!');
+      await exec.exec('git config --global user.name "gh-automation"');
+      await exec.exec('git config --global user.name "gh-automation@email.com"');
+      await exec.exec('git checkout -b ${targetBranch}', [], {
+        ...commonExecOpts
+      });
+      await exec.exec('git add package.json package-lock.json', [], {
+        ...commonExecOpts
+      });
+      await exec.exec('git commit -m "chore: update dependencies"', [], {
+        ...commonExecOpts
+      });
+      await exec.exec('git push -u origin ${targetBranch} --force', [], {
+        ...commonExecOpts
+      });
+      const octokit = github.getOctokit(ghToken);
+
+      try {
+      await octokit.rest.pulls.create({
+       owner: 'github.context.repo.owner',
+       repo: 'github.context.repo.repo',
+       tittle: 'Update NPM dependencies',
+       body: 'This pull request updates NPM packages',
+       base: baseBranch,
+       head: targetBranch
+      });
+    } catch (e) {
+      core.error('[js-dependency-update] : Something went wrong while creating the PR. Check logs below')
+      core.error(e.message);
+      core.setFailed();
+    }
+
     } else {
-        core.info('[js-dependency-update] : no updates at this point in time!')  
+        core.info('[js-dependency-update] : no updates at this point in time.');
     }
 
     /*
@@ -51,13 +88,13 @@ async function run() {
        1.2 target-branch to use to create the PR
        1.3 Github Token for Authentication purposes (to create PRs)
        1.4 Working directory for which to check for dependencies
-  [DONE]2. Execute the npm update command within the working directory
-  [DONE]3. Check whether there are modified package*.json files
-    4. If there are modified files:
+    [DONE] 2. Execute the npm update command within the working directory
+    [DONE] 3. Check whether there are modified package*.json files
+    [DONE] 4. If there are modified files:
       4.1 add and commit files to the target-branch
       4.2 create a PR to the base-branch using the octokit API
-      3.2 Otherwise, conclude the custom action
+      [DONE] 5. Otherwise, conclude the custom action
     */
-    core.info('I am custom JS action');
 }
+
 run();
